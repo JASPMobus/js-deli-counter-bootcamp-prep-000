8f1e60d6875a3c9176dcd7c9b1bc0e5b5396de65
James Mobus <mobusj@live.com>
2019-07-22 18:14:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(line) {
     var ret = "The line is currently:"
     
     for(let i=1;i<=line.length;i+=0) {
-      ret = ret + i + ". "
+      ret = ret + i + `. ${line[0]`
     }
   }
 }
