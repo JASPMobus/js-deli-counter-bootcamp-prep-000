0e40f7442f5bf919021f047b1c98d85980979ae2
James Mobus <mobusj@live.com>
2019-07-22 18:03:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(list, person) {
   list.push(person)
   var len = list.length;
-  return "Welcome ${person}, you are number ${len} in line."
+  return `Welcome ${person}, you are number ${len} in line.`
 }
